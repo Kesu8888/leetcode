e25a084a4dc046c11aafdd2c86e0cb01e516df9b
unknown <kevenfukaiqi@icloud.com>
2024-05-18 17:51:54 +0800

    leetcode10 1

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        leetcode9 l = new leetcode9();
-        boolean ans = l.sol(202);
+        leetcode10 l = new leetcode10();
+        boolean ans = l.sol("aaa", "ab*a*c*a");
         System.out.println(ans);
     }
 }
--- java/src/leetcode10.java
+++ java/src/leetcode10.java
@@ -5,12 +5,61 @@ public class leetcode10 {
 
         int i1 = src.length - 1;
         int i2 = ptn.length - 1;
-        while ((i1 >= 0) || (i2 >= 0)) {
+
+        int[] starChar = new int[]{ptn.length, 0};
+        while ((i1 >= 0) && (i2 >= 0)) {
             if (ptn[i2] == 42) {
-                
+                starChar[0] = i2;
+                int[] position = starChar(src, i1, ptn, i2);
+                starChar[1] = i1 - position[0];
+                i1 = position[0];
+                i2 = position[1];
+                continue;
             }
+            if (!compare(src[i1], ptn[i2])) {
+                if ()
+                return false;
+            }
+            i1 --;
+            i2 --;
         }
 
-        return false;
+        if (i1 < 0 && i2 < 0) {
+            return true;
+        }
+        if (i2 < 0) {
+            return false;
+        }
+        while (i2 >= 0) {
+            if (ptn[i2] == 42) {
+                i2 = i2 - 2;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean compare(int i1, int i2) {
+        if (i2 == 46) {
+            return true;
+        }
+        return i1 == i2;
+    }
+
+    public int[] starChar(char[] src, int i1, char[] ptn, int i2) {
+        int prev = ptn[i2 - 1];
+        int[] ret = new int[2];
+        while (i1 >= 0) {
+            if (!compare(src[i1], prev)) {
+                ret[0] = i1;
+                ret[1] = i2 - 2;
+                return ret;
+            }
+            i1 --;
+        }
+        ret[0] = i1;
+        ret[1] = i2 - 2;
+        return ret;
     }
 }
